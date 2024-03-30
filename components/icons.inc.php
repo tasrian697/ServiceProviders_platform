00a45f184a22367cae2d7e55939641a0b19bdae7
salma85 <salmatasrian85@gmail.com>
2024-03-31 05:30:06 +0600

    components

---
+++ components/icons.inc.php
@@ -0,0 +1,52 @@
+<section class="icons">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-4">
+            <div class="icon gradient mb-4">
+            <img
+              src="img/companies/location.png"
+              alt="location logo"
+              class="img-fluid"
+            /> 
+            </div>
+            <h2>Location</h2>
+            <p>
+            Dhaka Branch: <br>
+            House # 108, Road # 12, Block # E, Manama Urban Forest Center, Level # 4, Banani, Dhaka-1213, Bangladesh 
+            
+            </p>
+          </div>
+          <div class="col-md-4">
+            <div class="icon gradient mb-4">
+            <img
+              src="img/companies/blog.png"
+              alt="Company logo"
+              class="img-fluid"
+            /> 
+            </div>
+            <h2>Blog</h2>
+            <p class="mb-0">
+            Explore insightful articles, helpful tips.Stay updated with the latest news, guides, and stories crafted to enrich your knowledge and enhance your experience. 
+            </p>
+          </div>
+          <div class="col-md-4">
+            <div class="icon gradient mb-4">
+            <img
+              src="img/companies/socialmedia.png"
+              alt="Company logo"
+              class="img-fluid w-80"
+            /> 
+            </div>
+            <h3>Follow Us</h3>
+            <p class="mb-0">
+              Facebook <br>
+              Twitter<br>
+              Instagram<br>
+              LinkedIn<br>
+              YouTube
+              
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
