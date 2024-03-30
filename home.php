ef763b81715dcffd6c08e4eaaa1ff07085113a09
Salma Tasrian <121690946+salmatasrian85@users.noreply.github.com>
2024-03-31 05:38:59 +0600

    Homepage (#4)
    * components
    
    * Landing Page
    
    ---------
    
    Co-authored-by: salma85 <salmatasrian85@gmail.com>

---
+++ home.php
@@ -0,0 +1,6 @@
+<?php require('components/head.inc.php'); ?>
+<?php require('components/navbar.inc.php'); ?>
+<?php require('components/header.inc.php'); ?>
+<?php require('components/services.inc.php'); ?>
+<?php require('components/icons.inc.php'); ?>
+<?php require('components/footer.inc.php'); ?>
---
+++ services.php
@@ -0,0 +1,4 @@
+<?php require('components/head.inc.php'); ?>
+<?php require('components/navbar.inc.php'); ?>
+<?php require('components/services.inc.php'); ?>
+<?php require('components/footer.inc.php'); ?>
