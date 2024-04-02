ece11e30f435d66963858aeb0e1ce14191254ce8
salma85 <salmatasrian85@gmail.com>
2024-04-03 02:59:34 +0600

    login page

---
+++ components/Common_login_page.inc.php
@@ -0,0 +1,91 @@
+<section class="services gradient">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+            fill="#fff"
+            fill-opacity="1"
+            d="M0,224L48,213.3C96,203,192,181,288,154.7C384,128,480,96,576,117.3C672,139,768,213,864,208C960,203,1056,117,1152,101.3C1248,85,1344,139,1392,165.3L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+            ></path>
+        </svg>
+        <section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-4">
+      <h1 class="d-flex flex-column align-items-center justify-content-center "> Welcome</h1>
+        <h4 class="d-flex flex-column align-items-center justify-content">Join over 4,500,000 people who decided to get help and get happy with WellBeing.</h4>
+        <img src="img/Common_login_pic.png"
+          class="img-fluid" alt="Selfcare">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div class="d-flex flex-column align-items-center justify-content-center justify-content-lg-start">
+            <p class="lead fw-normal mb-0 me-3"> <h3>Get Started</h3></p>  
+          </div>
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+          <label class="form-label" for="form3Example3">Email address</label>
+            <input type="email" id="form3Example3" class="form-control form-control-lg"
+              placeholder="Enter a valid email address" />
+            
+          </div>
+          <div class="form-outline mb-3">
+          <div class="form-group">
+                  <div class="form-outline mb-3">
+                  <label class="form-label" for="form3Example3">User Type</label>
+                    <select class="form-select" aria-label="Default select example">
+                      <option value="" selected disabled hidden>Select User Type</option>
+                      <option value="patient">Mental Health Service Receiver</option>
+                      <option value="patient">Mental Health Service Provider</option>
+                      <option value="doctor">Job Seeker</option>
+                      <option value="caregiver">Employment Agency </option>
+                      <option value="caregiver">Housing Assistance Seeker </option>
+                      <option value="caregiver">Housing Assistance Agency</option>
+
+                    </select>
+                  </div>
+                </div>
+            </div>
+
+          <!-- Password input -->
+          <div class="form-outline mb-3">
+          <label class="form-label" for="form3Example4">Password</label>
+            <input type="password" id="form3Example4" class="form-control form-control-lg"
+              placeholder="Enter password" />
+            
+          </div>
+
+          <div class="d-flex justify-content-between align-items-center">
+            <!-- Checkbox -->
+            <div class="form-check mb-0">
+              <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+              <label class="form-check-label" for="form2Example3">
+                Remember me
+              </label>
+            </div>
+            <a href="#!" class="text-body">Forgot password?</a>
+          </div>
+
+          <div class="text-center text-lg-start mt-4 pt-2">
+          <button type="button" class="btn btn-primary btn-md" style="">
+                <a class="nav-link" href="mhealth_dashboard.php" style="color: white;">Login</a></button>
+            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+                class="link-danger">Register</a></p>
+          </div>
+          <div class="mb-3"></div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+</section>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 210">
+        <path
+          fill="#fff"
+          fill-opacity="1"
+          d="M0,96L34.3,106.7C68.6,117,137,139,206,122.7C274.3,107,343,53,411,53.3C480,53,549,107,617,117.3C685.7,128,754,96,823,96C891.4,96,960,128,1029,154.7C1097.1,181,1166,203,1234,202.7C1302.9,203,1371,181,1406,170.7L1440,160L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
+        ></path>
+      </svg>    
+
+</section>
+    <div class="container">
+
+    </div>
