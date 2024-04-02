204c2b02f069d713f72e189c78aa09526713fa30
salma85 <salmatasrian85@gmail.com>
2024-04-03 03:53:36 +0600

    formatted

--- components/footer.inc.php
+++ components/footer.inc.php
@@ -1,10 +1,9 @@
 <footer class="gradient">
-      <div class="container-fluid text-center">
-        <span
-          >Copyright © 2024 WellBeing All Rights Reserved.</span
-        >
-      </div>
-    </footer>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+  <div class="container-fluid text-center">
+    <span>Copyright © 2024 WellBeing All Rights Reserved.</span>
+  </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+</body>
+
 </html>
