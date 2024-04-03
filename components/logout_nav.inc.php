736615846e2bc218c3275083776662c77ab3db13
salma85 <salmatasrian85@gmail.com>
2024-04-03 15:37:07 +0600

    logout

---
+++ components/logout_nav.inc.php
@@ -0,0 +1,6 @@
+<div class="col-lg-12 d-flex justify-content-end">
+  <div class="search-box mx-4">
+    <input type="text" placeholder="Search">
+  </div>
+  <button class="btn btn-primary">Log Out</button>
+</div>
