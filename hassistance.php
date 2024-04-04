cc1bdb69121886753cd64433a1efca64bf24d3fe
Md Ariful Shifat <shifat98ariful@gmail.com>
2024-04-05 00:50:52 +0600

    hassistance

--- hassistance.php
+++ hassistance.php
@@ -1,4 +1,4 @@
-<?php require('components/logout_nav.inc.php'); ?>
+<?php require('components/nav_logout.inc.php'); ?>
 <?php require('components/hassistance_nav.inc.php'); ?>
 <?php require('components/head.inc.php'); ?>
 <?php require('components/hassistance_dashboard.inc.php'); ?>
