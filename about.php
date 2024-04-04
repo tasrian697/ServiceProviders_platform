649355f09badff62ec6f6abf8d1bfea154731169
salma85 <salmatasrian85@gmail.com>
2024-04-04 17:13:30 +0600

    phpfile

---
+++ about.php
@@ -0,0 +1,4 @@
+<?php require('components/head.inc.php'); ?>
+<?php require('components/navbar.inc.php'); ?>
+<?php require('components/about.inc.php'); ?>
+<?php require('components/footer.inc.php'); ?>
