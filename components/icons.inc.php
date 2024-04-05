bafe0735866feb52e69b5bf2159d6018eb53b918
salma85 <salmatasrian85@gmail.com>
2024-04-06 02:26:33 +0600

    icon div

--- components/icons.inc.php
+++ components/icons.inc.php
@@ -1,4 +1,9 @@
 <section class="icons">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-md-4">
+        <div class="icon gradient mb-4">
+          <img src="img/companies/location.png" alt="location logo" class="img-fluid" />
         </div>
         <h2>Location</h2>
         <p>
