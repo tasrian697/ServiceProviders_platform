59f1cffea476f5d443e0e8702b649bcfcf326afd
salma85 <salmatasrian85@gmail.com>
2024-04-16 01:34:13 +0600

    updated

---
+++ components/emp_rsc_jobs.inc.php
@@ -0,0 +1,47 @@
+<section class="features">
+    <div class="feature-container">
+
+        <div class="feature-box">
+            <div class="f-img">
+                <img src="img/skill.jpg" />
+            </div>
+            <div class="f-text">
+                <h4>Graphic Designer</h4>
+                <p>MK Apparels Ltd<br>
+                Work at office <br>
+                Barishal (Barishal Sadar)</p>
+                <button type="button" class="btn btn-primary btn-sm">Apply Now
+                </button>
+            </div>
+        </div>
+
+        <div class="feature-box">
+            <div class="f-img">
+                <img src="img/skill2.jpg" />
+            </div>
+            <div class="f-text">
+                <h4>Web Developer</h4>
+                <p>Gprojukti.com Ltd<br>
+                    Work at office <br>
+                    Dhaka (Banani)</p>
+                <button type="button" class="btn btn-primary btn-sm">Apply Now
+                </button>
+
+            </div>
+        </div>
+
+        <div class="feature-box">
+            <div class="f-img">
+                <img src="img/skill3.jpg" />
+            </div>
+            <div class="f-text">
+                <h4>Project Manager</h4>
+                <p>Kusholi Nirmata Ltd.<br>
+                    Full Time <br>
+                Anywhere in Bangladesh</p>
+                <button type="button" class="btn btn-primary btn-sm">Apply Now
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
