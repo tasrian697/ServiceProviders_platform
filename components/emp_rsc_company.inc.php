dbff5a373ebdaa4e731c995227ad9de7e9762555
salma85 <salmatasrian85@gmail.com>
2024-04-16 01:02:06 +0600

    updated

--- components/emp_rsc_company.inc.php
+++ components/emp_rsc_company.inc.php
@@ -5,30 +5,27 @@
 <table class="table">
   <thead class="thead-light">
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">SL.</th>
+      <th scope="col">Company Name</th>
+      <th scope="col">No. of jobs</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>Yellow Bird</td>
+      <td>1</td>
+
     </tr>
     <tr>
       <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <td>Giant Group</td>
+      <td>4</td>
     </tr>
     <tr>
       <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <td>Global_Vision</td>
+      <td>1</td>
     </tr>
   </tbody>
 </table>
